c6eeaac298ef5226b6facc9bd9a9f225b2d695a1
liyongxing86 <18209262612@163.com>
2019-06-29 11:31:08 +0800

    second2

---
+++ springboot_zuul/src/main/java/com/qf/lyx/SpringbootZuulApplication.java
@@ -0,0 +1,13 @@
+package com.qf.lyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootZuulApplication.class, args);
+	}
+
+}
